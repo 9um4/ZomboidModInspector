7473eccc9522b61304c7cdfce377e28515ab7dff
9um4 <51898564+9um4@users.noreply.github.com>
2023-10-03 22:28:03 +0900

    add: WorkShop Class

--- src/main/java/io/github/vitelotte/zomboid/mod/WorkShop.java
+++ src/main/java/io/github/vitelotte/zomboid/mod/WorkShop.java
@@ -1,2 +1,11 @@
-package io.github.vitelotte.zomboid.mod;public class WorkShop {
+package io.github.vitelotte.zomboid.mod;
+
+public class WorkShop {
+    private String workShopName;
+    private String workShopLink;
+
+    public WorkShop(String workShopName, String workShopLink) {
+        this.workShopName = workShopName;
+        this.workShopLink = workShopLink;
+    }
 }
