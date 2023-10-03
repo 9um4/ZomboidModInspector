412c2b66831d6256cd95bb989bef6f623fd6db73
9um4 <51898564+9um4@users.noreply.github.com>
2023-10-03 22:28:16 +0900

    add: Mod Class

--- src/main/java/io/github/vitelotte/zomboid/mod/Mod.java
+++ src/main/java/io/github/vitelotte/zomboid/mod/Mod.java
@@ -1,4 +1,30 @@
-package io.github.vitelotte.zomboid.mod.base;
+package io.github.vitelotte.zomboid.mod;
+
+import io.github.vitelotte.zomboid.mod.enums.Category;
+
+import java.util.Set;
 
 public class Mod {
+    private WorkShop workShop;
+    private String packageName;
+    private Set<Category> categories;
+    private Set<Mod> dependencies;
+
+    private Set<Mod> incompatibles;
+    private String explanations;
+
+    public Mod(WorkShop workShop,
+               String packageName,
+               Set<Category> categories,
+               Set<Mod> dependencies,
+               Set<Mod> incompatibles,
+               String explanations)
+    {
+        this.workShop = workShop;
+        this.packageName = packageName;
+        this.categories = categories;
+        this.dependencies = dependencies;
+        this.incompatibles = incompatibles;
+        this.explanations = explanations;
+    }
 }
