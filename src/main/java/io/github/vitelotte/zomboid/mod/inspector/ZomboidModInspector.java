7dcdb49022b5d7430a66f7b1138930c508488f0b
9um4 <51898564+9um4@users.noreply.github.com>
2023-10-03 22:02:13 +0900

    Initial commit

---
+++ src/main/java/io/github/vitelotte/zomboid/mod/inspector/ZomboidModInspector.java
@@ -0,0 +1,7 @@
+package io.github.vitelotte.zomboid.mod.inspector;
+
+public class ZomboidModInspector {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
