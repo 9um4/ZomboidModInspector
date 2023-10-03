f67f09f58764045f45132ac0e081dcae920a6514
9um4 <51898564+9um4@users.noreply.github.com>
2023-10-04 00:06:54 +0900

    add: Add WorkShopList which implements List Class

---
+++ src/main/java/io/github/vitelotte/zomboid/mod/collections/WorkShopList.java
@@ -0,0 +1,135 @@
+package io.github.vitelotte.zomboid.mod.collections;
+
+import io.github.vitelotte.zomboid.mod.WorkShop;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class WorkShopList implements List<WorkShop> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<WorkShop> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(WorkShop workShop) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends WorkShop> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends WorkShop> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public WorkShop get(int index) {
+        return null;
+    }
+
+    @Override
+    public WorkShop set(int index, WorkShop element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, WorkShop element) {
+
+    }
+
+    @Override
+    public WorkShop remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<WorkShop> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<WorkShop> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<WorkShop> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
